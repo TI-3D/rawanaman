9b275b2c3a2ee22b0a4e8185b4cba8c4d2c6085f
mkemaln <kemalmuhammad2509@gmail.com>
2024-10-21 12:49:35 +0700

    add navbar and faicon package

---
+++ lib/widgets/navbar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Navbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.seedling),
+            label: 'My Plant',
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.magnifyingGlass),
+            label: 'Find Plant',
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.book),
+            label: 'Wiki Plants',
+          ),
+        ],
+        // currentIndex: _selectedIndex,
+        // selectedItemColor: Colors.amber[800],
+        // onTap: _onItemTapped,
+      ),
+    );
+  }
+}
